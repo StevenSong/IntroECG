b5129780865a4af92ff31c09b743539d2a58dc7a
Steven Song <steven.song98@gmail.com>
2025-12-15 20:49:32 -0600

    only do inference in docker run script

--- 7-EchoNext Minimodel/runall.sh
+++ 7-EchoNext Minimodel/runall.sh
@@ -5,21 +5,21 @@ CHECKPOINT="$MODEL_DIR/weights.pt"
 TABULAR_PIPELINE="$MODEL_DIR/tabular_transformer.joblib"
 WAVEFORM_PARAMS="$MODEL_DIR/waveform_normalization_params.json"
 
-python parse_xml.py \
-    --xml_dir /xml_input \
-    --out_dir /processed_data \
-    --n_jobs 40
+# python parse_xml.py \
+#     --xml_dir /xml_input \
+#     --out_dir /processed_data \
+#     --n_jobs 40
 
-python preprocess.py \
-    --ecgmeta_path /processed_data/echonext_ecg_metadata.parquet \
-    --out_dir /processed_data \
-    --tabular_pipeline_path "$TABULAR_PIPELINE" \
-    --waveform_params_path "$WAVEFORM_PARAMS" \
-    --n_jobs 20
+# python preprocess.py \
+#     --ecgmeta_path /processed_data/echonext_ecg_metadata.parquet \
+#     --out_dir /processed_data \
+#     --tabular_pipeline_path "$TABULAR_PIPELINE" \
+#     --waveform_params_path "$WAVEFORM_PARAMS" \
+#     --n_jobs 20
 
 python cradlenet/scripts/inference/ecg_tabular.py \
-  --features_path /processed_data/waveforms.npy \
-  --tabular_path /processed_data/tabular_features.npy \
+  --features_path /processed_data/EchoNext_test_waveforms.npy \
+  --tabular_path /processed_data/EchoNext_test_tabular_features.npy \
   --legacy echonext \
   --batch_size 256 \
   --num_workers 2 \
@@ -31,8 +31,8 @@ python cradlenet/scripts/inference/ecg_tabular.py \
   --binary \
   --checkpoint "$CHECKPOINT"
 
-python run_metrics.py \
-  --ecgmeta_path /processed_data/ecg_metadata_final.parquet \
-  --labels_path /xml_input/labels.csv \
-  --prediction_path /results/prediction_loop/probs.npy \
-  --output_dir /results
+# python run_metrics.py \
+#   --ecgmeta_path /processed_data/ecg_metadata_final.parquet \
+#   --labels_path /xml_input/labels.csv \
+#   --prediction_path /results/prediction_loop/probs.npy \
+#   --output_dir /results
